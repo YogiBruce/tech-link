69ad8dd30e5d4e08f6629f2e90c09b6e9ba7ec3b
YogiBruce <108956598+YogiBruce@users.noreply.github.com>
2022-11-19 08:04:36 -0800

    user dashboard

---
+++ controllers/user/dashboard.js
@@ -0,0 +1,19 @@
+const router = require('express').Router();
+const isAuthenticated = require('../../utils/auth');
+const { Post, User } = require('../../models/index');
+
+//show user posts when logged in
+router.get('/', isAuthenticated, async (req, res) => {
+    try {
+        const postData = await Post.findAll(
+            {
+                where: { user_id: req.user.id },
+                include: { model: User }
+            });
+    }
+    catch (err) {
+        res.status(400).send(err.errors.map(e => e.message));
+    }
+});
+
+module.exports = router;
