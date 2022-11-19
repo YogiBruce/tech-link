03de44a64fa1c3e64e9d924abab463d2d0cc7683
YogiBruce <108956598+YogiBruce@users.noreply.github.com>
2022-11-19 10:46:56 -0800

    post model attributes

--- models/Post.js
+++ models/Post.js
@@ -6,16 +6,30 @@ class Post extends Model { }
 Post.init(
     {
         id: {
-
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+            allowNull: false
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false
         },
         content: {
-
+            type: DataTypes.STRING,
+            allowNull: false
         },
         user_id: {
-
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: 'user',
+                key: 'id'
+            }
         },
-        comment_time: {
-            
+        post_time: {
+            type: DataTypes.DATEONLY,
+            defaultValue: DataTypes.NOW
         }
     },
     {
