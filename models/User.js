cff298caf52267e9504844105946cec062681eaf
YogiBruce <108956598+YogiBruce@users.noreply.github.com>
2022-11-19 09:37:57 -0800

    user model attributes

--- models/User.js
+++ models/User.js
@@ -15,17 +15,32 @@ class User extends Model {
 User.init(
     {
         id: {
-            
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+            allowNull: false
         },
         username:{
-
+            types: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                isAlphanumeric: true
+            }
         },
         email: {
-
+            type: DataTypes.STRING,
+            unique: true,
+            allowNull: false,
+            validate: {
+                isEmail: true
+            }
         },
         password: {
-
-        },
+            type: DataTypes.STRING(64),
+            validate: {
+                len: [8]
+            }
+        }
 
     },
     {
