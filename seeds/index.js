c8cef0ab2f0b103bab8d04a44f46d04ec6e1e30b
YogiBruce <108956598+YogiBruce@users.noreply.github.com>
2022-11-24 09:34:02 -0800

    seeds index

---
+++ seeds/index.js
@@ -0,0 +1,22 @@
+const sequelize = require('../config/connection');
+const seedUser = require('./userSeedData.json');
+const seedPost = require('./postSeedData.json');
+const seedComments = require('./commentSeedData.json');
+
+const seedDatabase = async () => {
+    await sequelize.sync({ force: true });
+    console.log("\n-----Database Syced-----\n");
+
+    await User.bulkCreate(seedUser, { individualHooks: true });
+    console.log("\n-----Users Seeded-----\n");
+
+    await Post.bulkCreate(seedPost, { individualHooks: true });
+    console.log("\n-----Posts Seeded-----\n");
+
+    await Comment.bulkCreate(seedComments, { individualHooks: true });
+    console.log("\n-----Comments Seeded-----\n");
+
+    process.exit(0);
+};
+
+seedDatabase();
