1686964881fb43d8997ce74116fd809788b7b9b9
YogiBruce <108956598+YogiBruce@users.noreply.github.com>
2022-11-24 13:49:25 -0800

    public login

--- public/js/login.js
+++ public/js/login.js
@@ -1,16 +1,54 @@
 //Login form
 const loginFormHandler = async (event) => {
+  event.preventDefault();
 
+  //Values from login form
+  const username = document.querySelector("#username-login").value.trim();
+  const password = document.querySelector("#password-login").value.trim();
+
+  if (username && password) {
+    //Send a POST request to the API endpoint
+    const response = await fetch("/api/users/login", {
+      method: "POST",
+      body: JSON.stringify({ username, password }),
+      headers: { "Content-Type": "application/json" },
+    });
+
+    if (response.ok) {
+      //If successful redirect the browser to the dashboard
+      document.location.replace("/dashboard");
+    } else {
+      alert(response.statusText);
+    }
+  }
 };
 
 //Signup form
 const signupFormHandler = async (event) => {
+  event.preventDefault();
 
+  //Values from signup form
+  const username = document.querySelector("#username-signup").value.trim();
+  const password = document.querySelector("#password-signup").value.trim();
+
+  if (username && password) {
+    const response = await fetch("/api/users", {
+      method: "POST",
+      body: JSON.stringify({ username, password }),
+      headers: { 'Content-Type': 'application/json' }
+    });
+
+    if (response.ok) {
+        document.location.replace('/dashboard/')
+    } else {
+        alert(response.statusText);
+    }
+  }
 };
 
 document
-    .querySelector('.login-form')
-    .addEventListener('submit', loginFormHandler);
+  .querySelector(".login-form")
+  .addEventListener("submit", loginFormHandler);
 document
-    .querySelector('.signup-form')
-    .addEventListener('submit', signupFormHandler);
+  .querySelector(".signup-form")
+  .addEventListener("submit", signupFormHandler);
