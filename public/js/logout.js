6db744edb3b849504d6dd50f592aaca8c8a75052
YogiBruce <108956598+YogiBruce@users.noreply.github.com>
2022-11-24 13:54:24 -0800

    logout js

---
+++ public/js/logout.js
@@ -0,0 +1,17 @@
+const logout = async (event) => {
+    event.preventDefault();
+
+    const response = await fetch('/api/users/logout', {
+        method: 'POST',
+        headers: {'Content-Type' : 'application/json'}
+    });
+
+    if (response.ok) {
+        document.location.replace('/')
+    } else {
+        alert.response.statusText
+    }
+}
+
+//Logout button
+document.querySelector('#logout').addEventListener('click', logoout);
