e8c3feb40728f6fd9d5ed450cb63673810ec7306
YogiBruce <108956598+YogiBruce@users.noreply.github.com>
2022-11-24 07:06:17 -0800

    server js fix

--- server.js
+++ server.js
@@ -1,37 +1,30 @@
+const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
-const passport = require('passport');
-require('./util/passport');
-
-//helper functions
-const morgan = require('morgan');
-const errorHandler = require('errorhandler');
+const helpers = require('./utils/helpers');
 
 //connects sesstion to sequeslize
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
-
-//handlebars
-const hbs = exphbs.create({});
-
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
 
-app.use(errorHandler());
-app.use(morgan('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended:false }));
-app.use(express.static(_dirname + '/public'));
+//handlebars
+const hbs = exphbs.create({ helpers });
+
 app.use(
     session({
-        secret: 'superstar',
-        cookie: {maxAge: 172800000, secure: false, sameSite: 'strict'},
+        secret: 'topSecret',
+        cookie: {
+            maxAge: 172800000,
+            httpOnly: true,
+            secure: false, 
+            sameSite: 'strict'
+        },
         resave: false,
         saveUninitialized: false,
         store: new SequelizeStore({
@@ -40,8 +33,12 @@ app.use(
     })
 );
 
-app.use(passport.initialize());
-app.use(passport.session());
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 //turn on routes
 app.use(routes);
@@ -49,7 +46,7 @@ app.use(routes);
 //turn on connection to db and server
 const start = async () => {
     await sequelize.sync();
-    app.listen(PORT, () => console.log(`server is listening to port ${PORT}`));
+    app.listen(PORT, () => console.log(`Server is listening to port ${PORT}`));
 };
 
-start();
+start()
