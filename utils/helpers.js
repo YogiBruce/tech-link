1a96cf59032c3f722f60f845bd291f147c60299c
YogiBruce <108956598+YogiBruce@users.noreply.github.com>
2022-11-24 07:59:34 -0800

    fix helpers

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,37 +1,8 @@
-const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
-const User = require('../models/User');
-
-passport.use(
-    new LocalStrategy(
-        {
-            usernameField: 'email',
-            passwordField: 'password',
-        },
-        async (email, password, done) => {
-            try {
-                const user = await User.findOne({ where: { email: email } });
-                if (!user) {
-                    return done(null, false, {
-                        message: 'email not registerd'
-                    });
-                }
-                const matchPassword = await user.checkPassword(password);
-                return matchPassword
-                ? done(null, user)
-                : done(null, false, { message: 'Incorrect password' });
-            }
-            catch (error) {
-                done(error);
-            }
-        }
-    )
-);
-
-passport.serializeUser(function(user, done) {
-    done(null, user.id);
-});
-
-passport.deserializeUser(function(id, done) {
-    User.findByPk(id).then(function(user) { done(null, user); });
-});
+module.exports = {
+    format_time: (date) => {
+        return date.toLocaleTimeString();
+    },
+    format_date: (date) => {
+        return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${new Date(date).getFullYear()}`
+    },
+};
