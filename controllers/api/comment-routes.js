0da84662108d57d4572f502269da26efa427c9e2
YogiBruce <108956598+YogiBruce@users.noreply.github.com>
2022-11-24 12:51:29 -0800

    comment route update

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -9,9 +9,10 @@ router.post('/', withAuth, async (req, res) => {
             user_id: req.body.user_id,
             post_id: req.body.post_id
         });
-        res.status(newComment);
+        res.status(200).json(newComment);
+    } catch (err) { 
+        res.status(400).json(err); 
     }
-    catch (err) { res.status(400).send('Failed to create new comment'); }
 });
 
 module.exports = router;
