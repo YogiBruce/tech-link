e09198580a90744db59d0b93567f80032199c761
YogiBruce <108956598+YogiBruce@users.noreply.github.com>
2022-11-24 13:28:47 -0800

    public js

---
+++ public/js/login.js
@@ -0,0 +1,16 @@
+//Login form
+const loginFormHandler = async (event) => {
+
+};
+
+//Signup form
+const signupFormHandler = async (event) => {
+
+};
+
+document
+    .querySelector('.login-form')
+    .addEventListener('submit', loginFormHandler);
+document
+    .querySelector('.signup-form')
+    .addEventListener('submit', signupFormHandler);
